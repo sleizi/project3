590ab6cf767b4a8cd47b9ab1ea7b474f1cc57304
Xuan Tu <cuusoinoi@gmail.com>
2022-03-09 18:40:21 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=tunx4
+export POSTGRES_PASSWORD=xuantu123
+export POSTGRES_HOST=tunx4-p3-db.c4yr6hji7zxd.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::tunx4-pe-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=tunx4
+export JWT_SECRET=hello
+export URL=http://localhost:8100
---
+++ udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'tunx4',
+  'password': 'xuantu123',
+  'database': 'postgres',
+  'host': 'tunx4-p3-db.c4yr6hji7zxd.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'tunx4',
+  'aws_media_bucket': 'tunx4-p3-dev',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret': 'hello',
+  },
+};
